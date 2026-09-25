f29ac014e82cefd3e54ee68615b54cc5023ceb6c
sazidbinmostafa <sazidbinmostafa@gmail.com>
2026-09-25 21:26:10 +0600

    Banner Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "@/components/home/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <Banner/>
+    </div>
   );
 }
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -9,7 +9,8 @@ function Navbar() {
     </>
 
     return (
-            <nav className="navbar bg-[#0C0D10] shadow-sm border-b-2 border-[#1A1B1F] text-white">
+        <nav className="text-white border-b-2 border-[#15171D] container">
+            <div className="navbar">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -21,18 +22,19 @@ function Navbar() {
                             {Links}
                         </ul>
                     </div>
-                    <Link href="/" className="btn btn-ghost text-xl flex items-center gap-2"><Image src="/icon.png" alt="FITLOG" width={25} height={25}/><h3>FITLOG</h3></Link>
+                    <Link href="/" className="btn btn-ghost text-xl flex items-center gap-2"><Image src="/icon.png" alt="FITLOG" width={25} height={25} /><h3>FITLOG</h3></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         {Links}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <Link href="/my-plan" className="btn btn-ghost group text-[#9CA3AF] focus:text-[#C2F800] active:text-[#C2F800] hover:text-[#C2F800] rounded-3xl">Plan <span className='badge group-focus:bg-[#C2F800] group-active:bg-[#C2F800] group-hover:bg-[#C2F800] border rounded-full text-sm text-[#9CA3AF] group-hover:text-black group-focus:text-black group-active:text-black'>{0}</span></Link>
-                    <Link href="/my-plan" className="btn btn-ghost group text-[#9CA3AF] focus:text-[#C2F800] active:text-[#C2F800] hover:text-[#C2F800] rounded-3xl">Saved <span className='badge group-focus:bg-[#C2F800] group-active:bg-[#C2F800] group-hover:bg-[#C2F800] border rounded-full text-sm text-[#9CA3AF] group-hover:text-black group-focus:text-black group-active:text-black'>{0}</span></Link>
+                <div className="navbar-end ml-2 flex gap-2">
+                    <Link href="/my-plan" className="btn btn-xs md:btn-md border border-[#15171D] btn-ghost group text-[#9CA3AF] focus:text-[#C2F800] active:text-[#C2F800] hover:text-[#C2F800] rounded-3xl">Plan <span className='badge group-focus:bg-[#C2F800] group-active:bg-[#C2F800] group-hover:bg-[#C2F800] border rounded-full text-sm text-[#9CA3AF] group-hover:text-black group-focus:text-black group-active:text-black'>{0}</span></Link>
+                    <Link href="/my-plan" className="btn btn-xs md:btn-md border border-[#15171D] btn-ghost group text-[#9CA3AF] focus:text-[#C2F800] active:text-[#C2F800] hover:text-[#C2F800] rounded-3xl">Saved <span className='badge group-focus:bg-[#C2F800] group-active:bg-[#C2F800] group-hover:bg-[#C2F800] border rounded-full text-sm text-[#9CA3AF] group-hover:text-black group-focus:text-black group-active:text-black'>{0}</span></Link>
                 </div>
-            </nav>
+            </div>
+        </nav>
     )
 }
 
